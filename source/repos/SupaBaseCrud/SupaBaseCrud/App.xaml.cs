ed6b3c253e1b741296638b0421fdcf722bc5215c
Luis Salamanca <luisguezguann@gmail.com>
2025-04-23 10:16:49 -0500

    Commit inicial del proyecto

---
+++ source/repos/SupaBaseCrud/SupaBaseCrud/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SupaBaseCrud
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ source/repos/SupaBaseCrud/SupaBaseCrud/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SupaBaseCrud
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
